948bc9953fe42ff72008418c5a83263274538d72
AY <ay.analyse@gmail.com>
2025-08-24 10:24:27 +0500

    Allow image.thum.io in next/image remotePatterns

--- next.config.js
+++ next.config.js
@@ -7,22 +7,28 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-		images: {
-			remotePatterns: [
-				{
-					protocol: 'https',
-					hostname: 'aceternity.com',
-					port: '',
-					pathname: '/images/products/thumbnails/new/**',
-				},
-				{
-					protocol: 'https',
-					hostname: 'lh3.googleusercontent.com',
-					port: '',
-					pathname: '/**',
-				},
-			],
-		},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'aceternity.com',
+				port: '',
+				pathname: '/images/products/thumbnails/new/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'image.thum.io',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
 };
 
 export default config;
